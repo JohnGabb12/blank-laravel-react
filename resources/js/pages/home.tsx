ee9fdfcb210775a41984065f4de424f8963ec327
JohnGabb12 <valencerina.johngabrielle@ue.edu.ph>
2025-12-01 07:15:19 +0800

    init

---
+++ resources/js/pages/home.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+
+export default function Home(): JSX.Element {
+    return (
+        <>
+            <div>
+                Nay Nay
+            </div>
+        </>
+    );
+}
